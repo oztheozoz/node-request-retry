ec47fd5834cb8348260b2d139ffb7dcf15c45ed4
Osbert Orr <osbert.orr@digitalinsight.com>
2016-09-02 15:39:48 -0700

    Add delay strategy

--- test/attempts.test.js
+++ test/attempts.test.js
@@ -2,6 +2,7 @@
 
 var request = require('../');
 var t = require('chai').assert;
+var sinon = require('sinon');
 
 describe('Request attempts', function () {
   it('should show 1 attempt after a successful call', function (done) {
@@ -23,4 +24,24 @@ describe('Request attempts', function () {
       done();
     });
   });
+
+  it('should call delay strategy 2 times after some retries', function (done) {
+    var mockDelayStrategy = sinon.stub().returns(100);
+    var startTime = process.hrtime();
+    request({
+      url: 'http://www.filltext.com/?rows=1&err=500', // return a 500 status
+      maxAttempts: 3,
+      retryDelay: 1000000, // Set to large delay to prove it will not be used
+      delayStrategy: mockDelayStrategy
+    }, function (err, response, body) {
+      var endTime = process.hrtime(startTime); // process.hrtime returns an array [seconds, nanoseconds]
+      var timeDiff = endTime[0] * 1000 + endTime[1]/1000000;
+
+      // If mockDelayStrategy returns 1ms, timeDiff would be ~560ms. Since it returns 100ms, we expect the timeDiff to be between 700 and 800 inclusive.
+      t.isTrue(timeDiff >= 700 && timeDiff <= 800, 'delayStrategy\'s return value was not used or was used incorrectly');
+      t.isTrue(mockDelayStrategy.calledTwice, 'mockDelayStrategy was called an incorrect amount of times'); // reason it's called 2 and not 3 times is because there is no delay for the first call
+      done();
+    });
+  });
 });
+
