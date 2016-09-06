60733982a5b067ea75f814bb3ce02ea49b8bb827
Osbert Orr <oztheozoz@gmail.com>
2016-09-05 20:29:48 -0700

    save

--- test/attempts.test.js
+++ test/attempts.test.js
@@ -25,22 +25,37 @@ describe('Request attempts', function () {
     });
   });
 
-  it('should call delay strategy 2 times after some retries', function (done) {
-    var mockDelayStrategy = sinon.stub().returns(100);
-    var startTime = process.hrtime();
-    request({
-      url: 'http://www.filltext.com/?rows=1&err=500', // return a 500 status
-      maxAttempts: 3,
-      retryDelay: 1000000, // Set to large delay to prove it will not be used
-      delayStrategy: mockDelayStrategy
-    }, function (err, response, body) {
-      var endTime = process.hrtime(startTime); // process.hrtime returns an array [seconds, nanoseconds]
-      var timeDiff = endTime[0] * 1000 + endTime[1]/1000000;
+  it('should call delay strategy 2 times after some retries', function () {
+    var delayRequest = function(delayTime) {
+      var mockDelayStrategy = sinon.stub().returns(delayTime);
+      var startTime = process.hrtime();
+      return new Promise(function (resolve,reject) {
+        request({
+          url: 'http://www.filltext.com/?rows=1&err=500', // return a 500 status
+          maxAttempts: 3,
+          retryDelay: 1000000, // Set to large delay to prove it will not be used
+          delayStrategy: mockDelayStrategy
+        }, function (err, response, body) {
+          if (err) {
+            reject(new Error('error making request'));
+          } else {
+            var endTime = process.hrtime(startTime); // process.hrtime returns an array [seconds, nanoseconds]
+            var timeDiff = endTime[0] * 1000 + endTime[1]/1000000;
+            t.isTrue(mockDelayStrategy.calledTwice, 'mockDelayStrategy was called an incorrect amount of times'); // reason it's called 2 and not 3 times is because there is no delay for the first call
+            resolve(timeDiff);
+          }
+        });
+      });
+    };
 
-      // If mockDelayStrategy returns 1ms, timeDiff would be ~560ms. Since it returns 100ms, we expect the timeDiff to be between 700 and 800 inclusive.
-      t.isTrue(timeDiff >= 700 && timeDiff <= 800, 'delayStrategy\'s return value was not used or was used incorrectly');
-      t.isTrue(mockDelayStrategy.calledTwice, 'mockDelayStrategy was called an incorrect amount of times'); // reason it's called 2 and not 3 times is because there is no delay for the first call
-      done();
+    var timeDiffFirstRequest = 0;
+    return delayRequest(1).then(function(timeDiff) {
+      timeDiffFirstRequest = timeDiff;
+      return delayRequest(100);
+    }).then(function(timeDiff) {
+      var timeDiffSecondRequest = timeDiff;
+      var totalDelayTime = timeDiffSecondRequest - timeDiffFirstRequest;
+      t.isTrue(totalDelayTime >= 150 && totalDelayTime <= 300, 'delayStrategy\'s delay time expected to be between 200 and 300 ms inclusive but was ' + totalDelayTime + 'ms');
     });
   });
 });
